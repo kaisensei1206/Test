bba6cd4bcd057b32394c560691b003c027ad6909
kaisensei1206 <willyfan2230@gmail.com>
2024-12-11 23:37:47 +0700

    index

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const fs = require('fs');
-const http = require('http')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('hello from the server !');
+});
