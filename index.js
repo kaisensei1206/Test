afc5a9a7d2b9e5a88056e172e4c408abd9ec8296
kaisensei1206 <willyfan2230@gmail.com>
2024-12-12 11:24:27 +0700

    add templates

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const http = require('http');
 
 
 
+//讀JSON檔 再把它轉成javascript
 
-const data =  fs.readFile('${__dirname}/dev-data/data.json', 'utf-8');
-const productData = JSON.parse(data); 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    res.end('hello from the server !');
-});//阿要先創建server
+    const pathName = req.url;
+    //阿要先創建server
 
-server.listen(8000, '127.0.0.1',() =>{
-    console.log("listen on prot 8000 ");
-}); //這server.listen就是開port server
+    server.listen(8000, '127.0.0.1', () => {
+        console.log("listen on prot 8000 ");
+    }); //這server.listen就是開port server
 
-if (pathName ==='/' || pathName ==='/overview'){
-    res.end('this is a overview');
-    
-}else if (pathName ==='/api'){
-    res.writeHead(200,{'content-type':'application/json'});
-    res.end(data);
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('this is a overview');
 
-}else if(pathName ==='/product'){
-    res.end('this is a product');
-}
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.end(data);
+
+    } else if (pathName === '/product') {
+        res.end('this is a product');
+    }
+});
