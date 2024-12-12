2f51f9c583a34d7e93712fc0a3daf32c61c50160
kaisensei1206 <willyfan2230@gmail.com>
2024-12-12 10:55:40 +0700

    index

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.end('hello from the server !');
 });
+
+server.listen(8000, '127.0.0.1',() =>{
+    console.log("listen on prot 8000 ");
+});
